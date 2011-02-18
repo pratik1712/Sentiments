d3d40a987dd31ac827df4b5df4a91a8caed815ab
Pratik Mishra <pratikmishra.1712@gmail.com>
2011-02-18 13:56:43 +0530

    Feb 18th

--- Main.py
+++ Main.py
@@ -1,23 +1,26 @@
 import nltk
 import MySQLdb
 
-
-def find_adverbs(sent):
+# extracting adverbs from the sentences
+def find_adverbs(tagged_words):
 	adv =[]
-	text = nltk.word_tokenize(sent)
-	tagged_words = nltk.pos_tag(text)
-	for word in tagged_words:
+	for index, word in enumerate(tagged_words):
 		if (word[1] == 'RB') | (word[1] == 'RBR') | (word[1] == 'RBS'):
-			adv.append(word[0])
+			value = []
+			value.append(index)
+			value.append(word[0])
+			adv.append(value)
 	return adv
 
-def find_adjectives(sent):
+# extracting adjectives from the sentences
+def find_adjectives(tagged_words):
 	adj =[]
-	text = nltk.word_tokenize(sent)
-	tagged_words = nltk.pos_tag(text)
-	for word in tagged_words:
+	for index, word in enumerate(tagged_words):
 		if (word[1] == 'JJ') | (word[1] == 'JJR') | (word[1] == 'JJS'):
-			adj.append(word[0])
+			value = []
+			value.append(index)
+			value.append(word[0])
+			adj.append(value)
 	return adj
 
 	
@@ -31,51 +34,64 @@ sql = "SELECT * FROM Polarity"
 Cursor.execute(sql)
 
 # Fetch all results from the cursor into a sequence and close the connection
+# results contains the database entries for list of adjectives
 results = Cursor.fetchall()
 Con.close()
 
-tokenizer=nltk.data.load('tokenizers/punkt/english.pickle')
-
+# input file
 fin = open("Input.txt","r")
 input_string = fin.read()
 fin.close()
 
+# spliting the paragraph to sentences
+tokenizer=nltk.data.load('tokenizers/punkt/english.pickle')
 sents = tokenizer.tokenize(input_string)
 
+# creating a 2-D array [sentences][words] for eaiser access
 sentence_collection = []
 for items in sents:
-	sentence_collection.append(nltk.word_tokenize(items))
+	sentence_collection.append(nltk.word_tokenize(items.strip('\n')))
 
-#	Collection of sentences and words ::
-#print sentence_collection[3][3]
+
+for sentence in sentence_collection:
+	# tagging words with POS
+	tagged_words = nltk.pos_tag(sentence)
 	
-
-for sent in sents:
-	sent = sent.strip('\n')
-	print sent
-	curr_val = 0	
-	#Dealing with adverbs
-	list_of_adverbs = find_adverbs(sent)
+	# extracing the list of adverbs and adjective and their position
+	adverbs_list = find_adverbs(tagged_words)
+	adjectives_list = find_adjectives(tagged_words)
 	
-	for adv in list_of_adverbs:
-		for item in results:
-			if (item[0].lower() == adv.lower()):
-				curr_val = curr_val + (item[1] * item[2])
-		
-		print "Adverb : " , adv 
+	# for storing the weight of a sentence
+	weightage = 0
 
-	# Dealing with adjectives
-	list_of_adjectives = find_adjectives(sent)
+	#print the original sentence
+	print sentence	
+
+	#looping all the adjectives for a sentence
+	for adj in adjectives_list:
+		print adj[1]
+
+		# comparing with the database
+		for entry in results:
+			if (adj[1] ==	entry[0]):
+				weightage = weightage + (entry[1] * entry[2])
+		# checking for adverb in the vicinity
+		for adv in adverbs_list:
+			# if the adverb is next to previous to word add 1 
+			# if there is a gap of a word in between add 0.5 to weight
+			val_1_pos = [adj[0] - 1 , adj[0] + 1]
+			val_2_pos = [adj[0] - 2 , adj[0] + 2]
+
+			if adv[0] in val_1_pos:
+				if weightage > 0:
+					weightage = weightage + 1
+				elif weightage < 0:
+					weightage = weightage - 1		
+			elif adv[0] in val_2_pos:
+				if weightage > 0:
+					weightage = weightage + 0.5
+				elif weightage < 0:
+					weightage = weightage - 0.5
 	
-	for adv in list_of_adjectives:
-		for item in results:
-			if (item[0].lower() == adv.lower()):
-				curr_val = curr_val + (item[1] * item[2])
-		
-		print "Adjective : " , adv 
-	
-	print "Weightage: " , curr_val
-
-	print
-
-
+	# the estimated weight of the sentence
+	print weightage
